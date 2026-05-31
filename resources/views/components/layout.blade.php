95a170c0a36a73093964295d90244fd8168564be
gludovatza <attila@gludovatz.hu>
2026-05-31 17:59:35 +0200

    Komplex sablon: teljeskörű integráció

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,10 +4,117 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    @vite('resources/css/app.css')
+    {{-- @vite('resources/css/main.css', 'resources/css/app.css') --}}
+    @vite('resources/sass/main.scss', 'resources/css/app.css')
     <title>Laravel Components</title>
 </head>
-<body>
-    {{ $slot }}
-</body>
+<body class="is-preload">
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Header -->
+					@include('includes._header')
+
+				<!-- Menu -->
+					@include('includes._menu')
+
+				<!-- Main -->
+					<div id="main">
+
+						<!-- Post -->
+							<article class="post">
+								<header>
+									<div class="title">
+										<h2><a href="{{ route('post') }}">Magna sed adipiscing</a></h2>
+										<p>Lorem ipsum dolor amet nullam consequat etiam feugiat</p>
+									</div>
+									<div class="meta">
+										<time class="published" datetime="2015-11-01">November 1, 2015</time>
+										<a href="#" class="author"><span class="name">Jane Doe</span><img src="{{ Vite::asset('resources/images/avatar.jpg') }}" alt="" /></a>
+									</div>
+								</header>
+								<a href="{{ route('post') }}" class="image featured"><img src="{{ Vite::asset('resources/images/pic01.jpg') }}" alt="" /></a>
+								<p>Mauris neque quam, fermentum ut nisl vitae, convallis maximus nisl. Sed mattis nunc id lorem euismod placerat. Vivamus porttitor magna enim, ac accumsan tortor cursus at. Phasellus sed ultricies mi non congue ullam corper. Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.</p>
+								<footer>
+									<ul class="actions">
+										<li><a href="{{ route('post') }}" class="button large">Continue Reading</a></li>
+									</ul>
+									<ul class="stats">
+										<li><a href="#">General</a></li>
+										<li><a href="#" class="icon solid fa-heart">28</a></li>
+										<li><a href="#" class="icon solid fa-comment">128</a></li>
+									</ul>
+								</footer>
+							</article>
+
+						<!-- Post -->
+							<article class="post">
+								<header>
+									<div class="title">
+										<h2><a href="{{ route('post') }}">Ultricies sed magna euismod enim vitae gravida</a></h2>
+										<p>Lorem ipsum dolor amet nullam consequat etiam feugiat</p>
+									</div>
+									<div class="meta">
+										<time class="published" datetime="2015-10-25">October 25, 2015</time>
+										<a href="#" class="author"><span class="name">Jane Doe</span><img src="{{ Vite::asset('resources/images/avatar.jpg') }}" alt="" /></a>
+									</div>
+								</header>
+								<a href="{{ route('post') }}" class="image featured"><img src="{{ Vite::asset('resources/images/pic02.jpg') }}" alt="" /></a>
+								<p>Mauris neque quam, fermentum ut nisl vitae, convallis maximus nisl. Sed mattis nunc id lorem euismod placerat. Vivamus porttitor magna enim, ac accumsan tortor cursus at. Phasellus sed ultricies mi non congue ullam corper.</p>
+								<footer>
+									<ul class="actions">
+										<li><a href="{{ route('post') }}" class="button large">Continue Reading</a></li>
+									</ul>
+									<ul class="stats">
+										<li><a href="#">General</a></li>
+										<li><a href="#" class="icon solid fa-heart">28</a></li>
+										<li><a href="#" class="icon solid fa-comment">128</a></li>
+									</ul>
+								</footer>
+							</article>
+
+						<!-- Post -->
+							<article class="post">
+								<header>
+									<div class="title">
+										<h2><a href="{{ route('post') }}">Euismod et accumsan</a></h2>
+										<p>Lorem ipsum dolor amet nullam consequat etiam feugiat</p>
+									</div>
+									<div class="meta">
+										<time class="published" datetime="2015-10-22">October 22, 2015</time>
+										<a href="#" class="author"><span class="name">Jane Doe</span><img src="{{ Vite::asset('resources/images/avatar.jpg') }}" alt="" /></a>
+									</div>
+								</header>
+								<a href="{{ route('post') }}" class="image featured"><img src="{{ Vite::asset('resources/images/pic03.jpg') }}" alt="" /></a>
+								<p>Mauris neque quam, fermentum ut nisl vitae, convallis maximus nisl. Sed mattis nunc id lorem euismod placerat. Vivamus porttitor magna enim, ac accumsan tortor cursus at. Phasellus sed ultricies mi non congue ullam corper. Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla. Cras vehicula tellus eu ligula viverra, ac fringilla turpis suscipit. Quisque vestibulum rhoncus ligula.</p>
+								<footer>
+									<ul class="actions">
+										<li><a href="{{ route('post') }}" class="button large">Continue Reading</a></li>
+									</ul>
+									<ul class="stats">
+										<li><a href="#">General</a></li>
+										<li><a href="#" class="icon solid fa-heart">28</a></li>
+										<li><a href="#" class="icon solid fa-comment">128</a></li>
+									</ul>
+								</footer>
+							</article>
+
+						<!-- Pagination -->
+							<ul class="actions pagination">
+								<li><a href="" class="disabled button large previous">Previous Page</a></li>
+								<li><a href="#" class="button large next">Next Page</a></li>
+							</ul>
+
+					</div>
+
+				<!-- Sidebar -->
+					@include('includes._sidebar')
+
+			</div>
+
+		<!-- Scripts -->
+			@include('includes._scripts')
+
+	</body>
 </html>
---
+++ resources/views/includes/_scripts.blade.php
@@ -0,0 +1,5 @@
+<script src="{{ Vite::asset('resources/js/jquery.min.js') }}" ></script>
+<script src="{{ Vite::asset('resources/js/browser.min.js') }}"></script>
+<script src="{{ Vite::asset('resources/js/breakpoints.min.js') }}"></script>
+<script src="{{ Vite::asset('resources/js/util.js') }}"></script>
+<script src="{{ Vite::asset('resources/js/main.js') }}"></script>
